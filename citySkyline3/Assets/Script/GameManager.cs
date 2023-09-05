4f071c2426b613251e272623928e3551a5a6e36e
Samfrdd <sam.frdd@eduge.ch>
2023-09-05 14:46:58 +0200

    update

--- citySkyline3/Assets/Script/GameManager.cs
+++ citySkyline3/Assets/Script/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
 
 
-    public List<GameObject> roadBlockPrefabs; // Liste de modèles de blocs
+    public List<GameObject> roadBlockPrefabs;
     public Vector2 terrainSize = new Vector2(100f, 100f);
     public Vector2 blockSize = new Vector2(10f, 10f);
     public int rows = 10;
@@ -19,15 +19,13 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        PlaceRoadBlocks();
+        PlaceRoadBlocksInGrid();
     }
 
-    void PlaceRoadBlocks()
+    void PlaceRoadBlocksInGrid()
     {
         float startX = -(terrainSize.x / 2) + (blockSize.x / 2);
         float startZ = -(terrainSize.y / 2) + (blockSize.y / 2);
-
-        GameObject previousBlock = null;
 
         for (int row = 0; row < rows; row++)
         {
@@ -38,65 +36,12 @@ public class GameManager : MonoBehaviour
                 Vector3 position = new Vector3(x, 0f, z);
 
                 GameObject selectedRoadBlockPrefab = roadBlockPrefabs[Random.Range(0, roadBlockPrefabs.Count)];
-
                 GameObject roadBlock = Instantiate(selectedRoadBlockPrefab, position, Quaternion.identity);
 
-                if (previousBlock != null)
-                {
-                    // Aligner le point de sortie du nouveau bloc avec le point d'entrée du bloc précédent
-                    // Assurez-vous de tenir compte de la rotation des blocs si nécessaire
-                    AlignRoadBlocks(previousBlock, roadBlock);
-                }
-                else
-                {
-                    // Connecter le point de sortie du bloc à un point de sortie en dehors de la zone de jeu
-                    ConnectToOutside(roadBlock);
-                }
-
-                previousBlock = roadBlock;
+                // Rotate the road block if necessary
+                // roadBlock.transform.Rotate(0f, Random.Range(0, 4) * 90f, 0f);
             }
         }
-    }
-
-    void AlignRoadBlocks(GameObject previousBlock, GameObject currentBlock)
-    {
-        // Récupérez les points de sortie/entrée des blocs
-        Transform previousExit = previousBlock.transform.Find("EntrancePoints");
-        Transform currentEntrance = currentBlock.transform.Find("ExitPoint");
-
-        if (previousExit != null && currentEntrance != null)
-        {
-            // Alignez le point de sortie du bloc précédent avec le point d'entrée du bloc courant
-            currentBlock.transform.position = previousExit.position - currentEntrance.localPosition + currentBlock.transform.position;
-        }
-        else
-        {
-            
-        }
-    }
-
-    void ConnectToOutside(GameObject roadBlock)
-    {
-        // Récupérez le point de sortie du bloc
-        Transform exitPoint = roadBlock.transform.Find("ExitPoint");
-
-        if (exitPoint != null)
-        {
-            // Placez le point de sortie à l'extérieur de la zone de jeu (par exemple, à la limite du terrain)
-            float terrainHalfWidth = terrainSize.x / 2;
-            float terrainHalfHeight = terrainSize.y / 2;
-
-            Vector3 exitPosition = exitPoint.position;
-            exitPosition.x = Mathf.Clamp(exitPosition.x, -terrainHalfWidth, terrainHalfWidth);
-            exitPosition.z = Mathf.Clamp(exitPosition.z, -terrainHalfHeight, terrainHalfHeight);
-
-            exitPoint.position = exitPosition;
-        }
-        else
-        {
-            
-        }
-        
     }
 
 
