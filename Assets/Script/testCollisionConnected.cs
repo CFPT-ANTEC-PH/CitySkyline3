721807a2cad7517e67609862405c2b43153b10ee
Samfrdd <sam.frdd@eduge.ch>
2023-09-11 13:07:40 +0200

    asdads

--- Assets/Script/testCollisionConnected.cs
+++ Assets/Script/testCollisionConnected.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class testCollisionConnected : MonoBehaviour
 {
 
-    public bool isConnected = false;
+    public bool isConnected;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log(" LA!");
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("UNE COL EST LA!");
         // Vérifier si la collision concerne un autre cube
         if (collision.gameObject.CompareTag("connected"))
         {
@@ -18,4 +31,6 @@ public class testCollisionConnected : MonoBehaviour
             // Vous pouvez effectuer ici d'autres actions en réponse à la collision
         }
     }
+
+
 }
